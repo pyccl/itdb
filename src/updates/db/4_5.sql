c38add0b9020a0068a72373cb8b0623e0ccb1af6
pyccl <changcongliang@163.com>
2026-05-19 21:30:34 +0800

    The interface has been updated, with new features added and bugs fixed.
    1. New Addition: Added department and employee functions. Departments and employees are temporarily used to display the owner (user) of devices, etc.
    2. Addition: Added a card feature. The overview card is used to display summary information on the homepage (cards can be customized in the card page).
    3. Addition: The operation log function has been added, which is not limited to SQL operations on the database. All system operations (such as login, logout, editing, etc.) will be recorded (the display page is not yet designed and will be added later).
    4. The homepage has been reorganized by category (when there are no cards, the top-level overview title is currently displayed, and a check for no overview will be added).
    5. In the Equipment page, a new condition has been added: If an unsupported software type is selected, the Software tab and other options will be hidden; similarly, the Cabinet option will also be hidden for non-rack-mounted devices.
    6. The device type has added a custom color function.
    7. Functions for duplicate detection have been added to hardware, types, etc. For example, alerts will be triggered when duplicate hardware serial numbers or internal IDs are detected.
    8. In the user interface editing section, a new database management password has been added (valid until 23:59:59 on the same day, allowing basic CRUD operations in the database management interface). The right side has been changed to a homepage card display selection function.
    9. In the department editing section, you can add new departments (including multi-level sub-departments). The right side displays the sub-departments and employees of the current department
    10. In the employee editing page, new employees can be added, and the currently managed hardware list will be displayed on the right side (enabling quick unbinding and binding)
    11. A new custom page size option has been added to the label printing page, enabling printing with compact label printers (e.g., Jingchen).
    12. In the report page, in addition to the existing pie chart, horizontal bar charts and vertical bar charts have been added.
    Other bug fixes and additions can be downloaded and run for review.

--- src/updates/db/4_5.sql
+++
@@ -1,4 +0,0 @@
-begin transaction;
-alter table labelpapers add column qrtext;   
-update settings set dbversion=5;
-commit;
--- src/updates/db/5_6.sql
+++
@@ -1,5 +0,0 @@
-begin transaction;
-alter table labelpapers add column wantnotext;
-alter table labelpapers add column wantraligntext;
-update settings set dbversion=6;
-commit;
